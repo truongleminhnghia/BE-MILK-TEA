d96f0f11e74fcc880ed9819cf40234fd06b8739a
NhatTrieu24 <nguyenhuunhattrieu2003@gmail.com>
2025-03-16 10:59:38 +0700

    update quantity, clear all

--- Business_Logic_Layer/Services/CartService.cs
+++ Business_Logic_Layer/Services/CartService.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
-using Business_Logic_Layer.DTO;
+//using Business_Logic_Layer.DTO;
 using Business_Logic_Layer.Models.Requests;
 using Business_Logic_Layer.Models.Responses;
 using Data_Access_Layer.Data;
